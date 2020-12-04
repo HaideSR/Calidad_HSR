db0df9c99e21718e432c65cd6c053427337759f9
Haide Senzano <haide.senzano@gmail.com>
2020-12-03 22:14:09 -0400

    primer commit

---
+++ tarea2/tarea_TDD/test/tarea_tdd/Tarea_TDDTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea_tdd;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Haide
+ */
+public class Tarea_TDDTest {
+    public Tarea_TDD fibo; 
+    public Tarea_TDDTest() {
+     fibo = new Tarea_TDD();   
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    /**
+     * Test of main method, of class Tarea_TDD.
+     */
+    @Test
+    public void testMain() {
+        int resultado= fibo.fibonacci(1);
+        assertEquals(1,resultado);
+    }
+    @Test
+    public void testMain2() {
+        int resultado= fibo.fibonacci(3);
+        assertEquals(4,resultado);
+        
+    }
+}
